ef25460399ee26a33ecd9e9ab63da81cd47cf96c
Beast Code <beastcode9999@gmail.com>
2022-08-07 19:11:49 +0900

    select necessary parts so far

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 const display = document.querySelector('.display');
-console.log(display)
+const result = document.querySelector('.result');
+const passwordLength = document.querySelector('.passwordLength > input')
+console.log(passwordLength)
+
