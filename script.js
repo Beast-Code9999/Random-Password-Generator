0ba111813cf67eb99f77325d996c0f412f78adb0
Beast Code <beastcode9999@gmail.com>
2022-08-09 21:28:49 +0900

    add choices.forEach add four checked variables

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 const display = document.querySelector('.display');
 const result = document.querySelector('.result');
-const passwordLength = document.querySelector('.passwordLength > input')
-const choices = Array.from(document.querySelectorAll('.choice > input'))
+const passwordLength = document.querySelector('.passwordLength > input');
+const choices = Array.from(document.querySelectorAll('.choice > input'));
 
+let uppercaseChecked = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let lowercaseChecked = "abscefghijklmnopqrstuvwxyz";
+let numbersChecked = "1234567890"
+let symbolsChecked = ""
+
+console.log(uppercaseChecked);
 
 choices.forEach(choice => {
-    choice.id
-    switch(choice.id) {
-        
-    }
+    console.log(choice.checked)
+    choice.addEventListener('click', () => {
+        if(choice.id === 'uppercase' && choice.checked) {
+            console.log("hello world");
+        }
+        else {
+            console.log("no world");
+        }
+    })
 })
