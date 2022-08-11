97f22c54a00b79622785552a3aabefc278d630b7
Beast <beastcode9999@gmail.com>
2022-08-11 14:42:14 +0900

    finish generatePassword function

--- script.js
+++ script.js
@@ -15,17 +15,17 @@ let currentPassLength = 8;
 let selectedChoices = [uppercaseString];
 
 // to listen for generate and copy password
-// window.addEventListener('click', function(event) {
-//     if(event.target.id === "generate") {
-//         console.log(currentPassLength)
-//         generatePassword(currentPassLength)
-//     }
+window.addEventListener('click', function(event) {
+    if(event.target.id === "generate") {
+        generatePassword(currentPassLength)
+    }
 
-//     if(event.target.id === "copy") {
-//         this.navigator.clipboard.writeText(result.textContent)
-//         this.alert("Text copied")
-//     }
-// })
+    if(event.target.id === "copy") {
+        this.navigator.clipboard.writeText(result.textContent)
+        this.alert("Text copied")
+    }
+    console.log(selectedChoices)
+})
 
 choices.forEach(choice => {
     choice.addEventListener('click', () => {
@@ -57,11 +57,20 @@ choices.forEach(choice => {
     })
 })
 
-function generatePassword(length, check) {
-    let password = ''
-    for(let i = 1; i <= length; i++) {
 
+
+function generatePassword(length) {
+    let password = ''
+    let joinedPass = selectedChoices.join('')
+    console.log(joinedPass)
+    if(selectedChoices.length === 0) result.textContent = "please select at least one option"
+    if(selectedChoices.length !== 0) {
+        for(let i = 1; i <= length; i++) {
+        password += joinedPass[Math.floor(Math.random() * joinedPass.length)]
+        }
+        result.textContent = password;
     }
+
 }
 
 passwordLength.addEventListener('change', function() {
