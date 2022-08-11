b319a18c2e50ead339cc4739d9f2d2ec313258c4
Beast <beastcode9999@gmail.com>
2022-08-11 13:52:58 +0900

    add new approach in new branch

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const display = document.querySelector('.display');
 const result = document.querySelector('.result');
-const passwordLength = document.querySelector('.passwordLength > input');
 const choices = Array.from(document.querySelectorAll('.choice > input'));
+const passwordLength = document.querySelector('.passwordLength > input');
 const generate = document.querySelector('.generate');
 
 const uppercaseString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -11,59 +11,36 @@ const symbolsString = "\`~!@#$%^&*()_+-=[]{}\\|;:\'\",<.>/?";
 
 // console.log(uppercaseString[Math.floor(Math.random() * uppercaseString.length)])
 
-let uppercaseChecked = true;
-let lowercaseChecked = false;
-let numbersChecked = false;
-let symbolsChecked = false;
 let currentPassLength = 8;
+let selectedChoices = [];
 
+// to listen for generate and copy password
+// window.addEventListener('click', function(event) {
+//     if(event.target.id === "generate") {
+//         console.log(currentPassLength)
+//         generatePassword(currentPassLength)
+//     }
 
-window.addEventListener('click', function(event) {
-    choices.forEach(function(choice) {
-        choice.addEventListener('click', function() {
-            checkChoices(choice.id, choice.checked) 
-        })
-    })
-        toConsole()
+//     if(event.target.id === "copy") {
+//         this.navigator.clipboard.writeText(result.textContent)
+//         this.alert("Text copied")
+//     }
+// })
+
+choices.forEach(choice => {
+    choice.addEventListener('click', () => {
+        (choice.id === 'uppercase' && choice.checked === true) ? selectedChoices.push(uppercaseString) : console.log(selectedChoices[0])
+
         console.log(selectedChoices)
-    if(event.target.id === "generate") {
-        console.log(currentPassLength)
-        generatePassword(currentPassLength)
-    }
-
-    if(event.target.id === "copy") {
-        this.navigator.clipboard.writeText(result.textContent)
-        this.alert("Text copied")
-    }
+    })
 })
-
-function toConsole() {
-    console.log(uppercaseChecked)
-    console.log(lowercaseChecked)
-    console.log(numbersChecked)
-    console.log(symbolsChecked)
-}
 
 function generatePassword(length, check) {
     let password = ''
     for(let i = 1; i <= length; i++) {
-        let random = Math.floor(Math.random() * 4 + 1);
-        if(random === 1 && uppercaseChecked === true) {
-            password += uppercaseString[Math.floor(Math.random() * uppercaseString.length)];
-        }
-        if(random === 2 && lowercaseChecked === true) {
-            password += lowercaseString[Math.floor(Math.random() * lowercaseString.length)];
-        }
-        if(random === 3 && numbersChecked === true) {
-            password += numbersString[Math.floor(Math.random() * numbersString.length)];
-        }
-        if(random === 4 && symbolsChecked === true) {
-            password += symbolsString[Math.floor(Math.random() * symbolsString.length)];
-        }
-    }
-    result.textContent = password;
-}
 
+    }
+}
 
 passwordLength.addEventListener('change', function() {
     console.log(this.value)
@@ -72,32 +49,4 @@ passwordLength.addEventListener('change', function() {
 
 
 
-function checkChoices(id, check) {
-    if(id === "uppercase" && check === true) {
-        uppercaseChecked = true;
-    } 
-    else if(id === "uppercase" && check === false) {
-        uppercaseChecked = false;
-        
-    }
-    if(id === "lowercase" && check === true) {
-        lowercaseChecked = true;
-    } 
-    else if(id === "lowercase" && check === false) {
-        lowercaseChecked = false;
-    }
-    if(id === "numbers" && check === true) {
-        numbersChecked = true;
-    } 
-    else if(id === "numbers" && check === false) {
-        numbersChecked = false;
-    }
-    if(id === "symbols" && check === true) {
-        symbolsChecked = true;
-    } 
-    else if(id === "symbols" && check === false) {
-        symbolsChecked = false;
 
-    }
-
-}
